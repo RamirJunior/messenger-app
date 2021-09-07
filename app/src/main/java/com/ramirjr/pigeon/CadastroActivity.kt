5cb45c4ffe2a60f816a8a13a9be35a32936e759b
Ramir Junior <junior.ribeiro0@live.com>
2021-09-06 22:37:37 -0300

    interacao com imagem no botao

--- app/src/main/java/com/ramirjr/pigeon/CadastroActivity.kt
+++ app/src/main/java/com/ramirjr/pigeon/CadastroActivity.kt
@@ -1,9 +1,18 @@
 package com.ramirjr.pigeon
 
 import android.content.Intent
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.ContextMenu
+import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.ramirjr.pigeon.databinding.ActivityMainBinding
@@ -14,6 +23,26 @@ class CadastroActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    @RequiresApi(Build.VERSION_CODES.P)
+    private val getContent = registerForActivityResult(
+        ActivityResultContracts
+            .GetContent()
+    ) { uriFotoEscolhida: Uri? ->
+        if (uriFotoEscolhida != null) {
+            configuraImageViewComImagemEscolhida(uriFotoEscolhida)
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
+    private fun configuraImageViewComImagemEscolhida(uriFotoEscolhida: Uri?) {
+        val source = ImageDecoder.createSource(this.contentResolver, uriFotoEscolhida!!)
+        val bitmap = ImageDecoder.decodeBitmap(source)
+        binding.ivFoto.visibility = VISIBLE
+        binding.ivFoto.setImageBitmap(bitmap)
+        binding.btnEscolherFoto.visibility = INVISIBLE
+    }
+
+    @RequiresApi(Build.VERSION_CODES.P)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -23,7 +52,11 @@ class CadastroActivity : AppCompatActivity() {
         }
 
         binding.btnEscolherFoto.setOnClickListener {
-            Log.d("photo:", "hora de escolher uma")
+            getContent.launch("image/*")
+        }
+
+        binding.ivFoto.setOnClickListener {
+            Log.d("ImageView", "clicou na imageview")
         }
 
         binding.textviewFacaLogin.setOnClickListener {
@@ -44,13 +77,6 @@ class CadastroActivity : AppCompatActivity() {
             Toast.makeText(this, "Por favor, digite E-mail/Senha.", Toast.LENGTH_SHORT).show()
             return true
         }
-        Log.d("main", "email: ${email}")
-        Log.d("main", "senha: ${senha}")
-
-        if (email.isEmpty() || senha.isEmpty()) {
-            Toast.makeText(this, "Por favor digite e-mail/senha.", Toast.LENGTH_SHORT).show()
-            return true
-        }
 
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, senha)
             .addOnCompleteListener {
@@ -64,6 +90,21 @@ class CadastroActivity : AppCompatActivity() {
                 Toast.makeText(this, "Falha ao criar usuario: ${it.message}", Toast.LENGTH_SHORT)
                     .show()
             }
+
+        if (binding.ivFoto.background == null) {
+            binding.ivFoto.setBackgroundResource(R.drawable.std_profile)
+        }
         return false
     }
+
+    override fun onCreateContextMenu(
+        menu: ContextMenu?,
+        v: View?,
+        menuInfo: ContextMenu.ContextMenuInfo?
+    ) {
+        super.onCreateContextMenu(menu, v, menuInfo)
+        menuInflater.inflate(R.menu.context_menu_image_profile, menu)
+    }
+
+
 }
