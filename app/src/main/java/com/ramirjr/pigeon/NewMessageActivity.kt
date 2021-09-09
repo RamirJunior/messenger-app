369d7468dfdd85e2908d44a9739073641f41d61c
Ramir Junior <junior.ribeiro0@live.com>
2021-09-09 11:40:14 -0300

    recycler com dados salvos no firebase

--- app/src/main/java/com/ramirjr/pigeon/NewMessageActivity.kt
+++ app/src/main/java/com/ramirjr/pigeon/NewMessageActivity.kt
@@ -2,12 +2,15 @@ package com.ramirjr.pigeon
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import com.ramirjr.pigeon.databinding.ActivityNewMessageBinding
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
@@ -21,13 +24,13 @@ class NewMessageActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         supportActionBar?.title = "Escolha um usuário"
-
-        val adapter = GroupAdapter<GroupieViewHolder>()
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-        adapter.add(UserItem())
-
-        binding.recyclerviewNewMessages.adapter = adapter
+//
+//        val adapter = GroupAdapter<GroupieViewHolder>()
+//        adapter.add(UserItem())
+//        adapter.add(UserItem())
+//        adapter.add(UserItem())
+//
+//        binding.recyclerviewNewMessages.adapter = adapter
 
         fetchUsers()
     }
@@ -59,7 +62,10 @@ class NewMessageActivity : AppCompatActivity() {
 
 class UserItem(val user: User) : Item<GroupieViewHolder>() {
     override fun bind(viewHolder: GroupieViewHolder, position: Int) {
-//    viewHolder.itemView.id.te
+        viewHolder.itemView.findViewById<TextView>(R.id.textview_username).text = user.username
+
+        Picasso.get().load(user.profileImageUrl)
+            .into(viewHolder.itemView.findViewById<ImageView>(R.id.imageview_new_message))
     }
 
     override fun getLayout(): Int {
